c1954e5af3073f2914103840dd89ad7253665fc8
RuslanKabachok <ruslan.kabachok@gmail.com>
2022-07-30 19:10:55 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,3 @@
-import user from 'user.json';
-import transactions from 'transaction.json';
-import friends from 'friends.json';
-import data from 'data.json';
-import { Profile } from 'components/Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { Friends } from './FriendsList/Friends';
-import { Transactions } from './TransactionHistory/TransactionHistory';
-
 export const App = () => {
   return (
     <div
@@ -19,18 +10,6 @@ export const App = () => {
         textTransform: 'uppercase',
         color: '#010101',
       }}
-    >
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
-      <Friends friends={friends} />
-      <Transactions items={transactions} />
-    </div>
+    ></div>
   );
 };
