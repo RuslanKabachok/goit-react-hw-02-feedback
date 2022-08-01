13c915a66f61c78ccebf96dd935f0a3d4aec5357
RuslanKabachok <ruslan.kabachok@gmail.com>
2022-08-01 18:48:57 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Widget from './components/Widget/Widget';
 import App from 'components/App';
 import './index.css';
 
@@ -8,6 +7,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <Widget /> */}
   </React.StrictMode>
 );
